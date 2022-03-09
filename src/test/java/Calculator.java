dbfee1f44db68d1b61aac260f86d73307cae6102
Anastasiya <nastya.bor.002@mail.ru>
2022-03-09 20:09:27 +0300

    Test for underweight Test for overweight Test for normal Test for starvation Test for obese

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -5,7 +5,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
 
 public class Calculator {
 
@@ -32,12 +33,94 @@ public class Calculator {
         driver.findElement(By.name("wg")).sendKeys("47");
         driver.findElement(By.name("ht")).sendKeys("168");
         driver.findElement(By.name("cc")).click();
-        boolean isDisplayed = driver.findElement(By.name("desk")).isDisplayed();
-        assertTrue(isDisplayed);
-
+        String actual = driver.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(actual, "Your category is Underweight", "Category does not match");
 
 
     }
+
+    @Test
+    public void Starvation(){
+        /*
+        1. Открыть драйвер
+        2. Зайти на сайт https://healthunify.com/bmicalculator/
+        3. В поле Weight ввести вес, например 34
+        4. В поле or cms ввести рост, например 168
+        5. Нажать кнопку Calculate
+        6. Проверить, что виден результат "Your category is Starvation"
+         */
+
+        driver.get("https://healthunify.com/bmicalculator/");
+        driver.findElement(By.name("wg")).sendKeys("34");
+        driver.findElement(By.name("ht")).sendKeys("168");
+        driver.findElement(By.name("cc")).click();
+
+        String actual = driver.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(actual, "Your category is Starvation", "Category does not match");
+    }
+
+    @Test
+    public void Normal(){
+        /*
+        1. Открыть драйвер
+        2. Зайти на сайт https://healthunify.com/bmicalculator/
+        3. В поле Weight ввести вес, например 56
+        4. В поле or cms ввести рост, например 168
+        5. Нажать кнопку Calculate
+        6. Проверить, что виден результат "Your category is Normal"
+         */
+
+        driver.get("https://healthunify.com/bmicalculator/");
+        driver.findElement(By.name("wg")).sendKeys("56");
+        driver.findElement(By.name("ht")).sendKeys("168");
+        driver.findElement(By.name("cc")).click();
+
+        String actual = driver.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(actual, "Your category is Normal", "Category does not match");
+    }
+
+    @Test
+    public void Overweight(){
+        /*
+        1. Открыть драйвер
+        2. Зайти на сайт https://healthunify.com/bmicalculator/
+        3. В поле Weight ввести вес, например 76
+        4. В поле or cms ввести рост, например 168
+        5. Нажать кнопку Calculate
+        6. Проверить, что виден результат "Your category is Overweight"
+         */
+
+        driver.get("https://healthunify.com/bmicalculator/");
+        driver.findElement(By.name("wg")).sendKeys("76");
+        driver.findElement(By.name("ht")).sendKeys("168");
+        driver.findElement(By.name("cc")).click();
+
+        String actual = driver.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(actual, "Your category is Overweight", "Category does not match");
+    }
+
+
+    @Test
+    public void Obese(){
+        /*
+        1. Открыть драйвер
+        2. Зайти на сайт https://healthunify.com/bmicalculator/
+        3. В поле Weight ввести вес, например 90
+        4. В поле or cms ввести рост, например 168
+        5. Нажать кнопку Calculate
+        6. Проверить, что виден результат "Your category is Obese"
+         */
+
+        driver.get("https://healthunify.com/bmicalculator/");
+        driver.findElement(By.name("wg")).sendKeys("90");
+        driver.findElement(By.name("ht")).sendKeys("168");
+        driver.findElement(By.name("cc")).click();
+
+        String actual = driver.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(actual, "Your category is Obese", "Category does not match");
+    }
+
+
 
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
